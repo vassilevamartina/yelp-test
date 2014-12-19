1207fbb58091fc6be46ebb1a7ed4ceef2c5eb73f
kmekler <kirby.lodin@gmail.com>
2014-12-19 00:03:06 -0800

    navbar fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,12 +84,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET'],
-    :access_key_id => ENV['S3_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['S3_BUCKET'],
+      :access_key_id => ENV['S3_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
+    }
   }
-}
 
 end
