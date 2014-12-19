2d24e3345f5aba60d308df70c488502eec770ad6
kmekler <kirby.lodin@gmail.com>
2014-12-18 16:06:49 -0800

    amazon s3 working

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,15 @@
 
   has_attached_file :image,
                 :styles => { :square => ['480x480#', :jpg] },
-                :default_url => 'https://chomp-app.herokuapp.com/',
                 :storage => :s3,
                 :bucket => 'chomp-app',
-                :s3_credentials => "#{Rails.root}/config/s3.yml"
+                :url => ':s3_domain_url',
+                :path => ':class/:attachment/:id/:style/:filename',
+                :s3_credentials => {
+                  :bucket => ENV['s3_bucket'],
+                  :access_key_id => ENV['s3_access_key_id'],
+                  :secret_access_key => ENV['s3_secret_access_key']
+                }
+  
+               
 end
--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,7 +1,7 @@
- if Rails.env == "production"
+# if Rails.env == "production"
      # set credentials from ENV hash
- S3_CREDENTIALS = { :access_key_id => ENV['s3_access_key_id'], :secret_access_key => ENV['s3_secret_access_key'], :bucket => "chomp-app"}
- else
+ #S3_CREDENTIALS = { :access_key_id => ENV['s3_access_key_id'], :secret_access_key => ENV['s3_secret_access_key'], :bucket => "chomp-app"}
+ #else
      # get credentials from YML file
-     S3_CREDENTIALS = Rails.root.join("config/s3.yml")
- end
+  #   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
+ #end
